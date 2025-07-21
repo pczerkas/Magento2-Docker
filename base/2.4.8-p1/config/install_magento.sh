a845b28872372d011ac9abb55755072181831a49
Przemysław Czerkas <pczerkas@gmail.com>
2025-07-21 18:42:21 +0200

    add more Magento versions

--- base/2.4.8-p1/config/install_magento.sh
+++ base/2.4.8-p1/config/install_magento.sh
@@ -109,8 +109,6 @@ else
         --currency=EUR \
         --timezone=Europe/Amsterdam \
         --use-rewrites=1 \
-        --use-secure=0 \
-        --use-secure-admin=0 \
         --search-engine=elasticsearch7 \
         --elasticsearch-host=$ELASTICSEARCH_SERVER \
         --elasticsearch-port=$ELASTICSEARCH_PORT \
